a7601549a01cbe9cfd846b8ebe7b04ce077e0676
Reader <doitreader@gmail.com>
2020-07-03 04:56:21 +0900

    Updates

---
+++ precache-manifest.6aa335912ae2db4b284ac4a7491d871a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1be25a12e43d85c4c919cf202fec943b",
+    "url": "/movie_app_reader/index.html"
+  },
+  {
+    "revision": "e1171feff96fc32ea721",
+    "url": "/movie_app_reader/static/css/main.30a787b5.chunk.css"
+  },
+  {
+    "revision": "617baa7c064388626fa5",
+    "url": "/movie_app_reader/static/js/2.e89451b1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_reader/static/js/2.e89451b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1171feff96fc32ea721",
+    "url": "/movie_app_reader/static/js/main.d3e5a7e6.chunk.js"
+  },
+  {
+    "revision": "33104543faf815f60952",
+    "url": "/movie_app_reader/static/js/runtime-main.2761a504.js"
+  }
+]);
